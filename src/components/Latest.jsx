54431a68fb1d0d70e29dfae4f7b97d0b214080c2
Cyri0 <nemestamas94@gmail.com>
2024-03-25 17:46:53 +0100

    LatestCard created and mapped

--- src/components/Latest.jsx
+++ src/components/Latest.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Latest = () => {
+  const [apiData, setApiData] = useState([])
+  
+  useEffect(()=>{
+    fetch("http://localhost:8000/latest.json")
+    .then(res => res.json())
+    .then(data => setApiData(data))
+  }, [])
+
   return (
-    <div>Latest</div>
+    <section id="latest">
+      <div className="wrapper">
+        <h2>Latest</h2>
+        <div className="cards">
+
+          { apiData.map((data, idx) => <LatestCard key={idx} {...data} />) }
+
+        </div>
+      </div>
+    </section>
   )
+}
+
+const LatestCard = (props) => {
+  return (<div className='latest-card'>
+    <img src="" alt={props.title} />
+    <h6>By {props.creator}</h6>
+    <h3>{props.title}</h3>
+    <p>{props.content}</p>
+  </div>)
 }
 
 export default Latest
